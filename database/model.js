9c53a44d0942dc793c96d86e3e945480d15377f8
nizarhalloun <nshalloun@hotmail.com>
2020-11-17 17:12:58 +0200

    changed database

---
+++ database/model.js
@@ -0,0 +1,18 @@
+const db = require('./connection');
+
+function getAllPosts() {
+  return db.query('SELECT * FROM trade_posts').then((res) => res.rows);
+}
+
+function getUserWithEmail(email) {
+  return db
+    .query(`SELECT * FROM users WHERE email='${email}'`)
+    .then((res) => res.rows);
+}
+
+function getAllPostFromUserId(id) {
+  return db
+    .query(`SELECT * FROM trade_posts WHERE user_id=${id}`)
+    .then((res) => res.rows);
+}
+module.exports = { getAllPosts, getUserWithEmail, getAllPostFromUserId };
