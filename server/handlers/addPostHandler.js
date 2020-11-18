d9df9abb84121f550f79643b007d66d0f5da8832
nizarhalloun <nshalloun@hotmail.com>
2020-11-18 17:20:47 +0200

    nizar

--- server/handlers/addPostHandler.js
+++ server/handlers/addPostHandler.js
@@ -1,15 +1,23 @@
-const models = require("../../database/models");
-const redirectHandler = require("./redirectHandler");
+const models = require('../../database/models');
+const redirectHandler = require('./redirectHandler');
 
 function addPostHandler(request, response) {
-  let body = "";
-  request.on("data", (chunk) => (body += chunk));
-  request.on("end", () => {
+  let body = '';
+  request.on('data', (chunk) => (body += chunk));
+  request.on('end', () => {
+    console.log(body);
     models.insertNewUser(JSON.parse(body));
+    response.writeHead('content-type', 'application/json');
+
+    response.end(
+      JSON.stringify({
+        msg: 'all done',
+      })
+    );
   });
-  request.on("error", (err) => {
+  request.on('error', (err) => {
     console.error(err);
-    redirectHandler(response, "/serverError");
+    redirectHandler(response, '/serverError');
   });
 }
 
