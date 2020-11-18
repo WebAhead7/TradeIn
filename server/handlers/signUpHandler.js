92604bc07f947629a4ec7a63a582aab62be26b18
nizarhalloun <nshalloun@hotmail.com>
2020-11-18 14:04:13 +0200

    fix

--- server/handlers/signUpHandler.js
+++ server/handlers/signUpHandler.js
@@ -1,51 +1,61 @@
-const fs = require("fs");
-const path = require("path");
-const models = require("../models");
+const fs = require('fs');
+const path = require('path');
+const models = require('../../database/models');
 
 function signUpHandler(request, response) {
-  let body = "";
-  request.on("data", (chunk) => {
+  let body = '';
+  request.on('data', (chunk) => {
     body += chunk;
   });
-  request.on("end", () => {
+  request.on('end', () => {
     console.log(body);
     // const userParams = new URLSearchParams(body);
     const { email, username, password, location } = JSON.parse(body);
-    console.log({ email, username, password, location });
+    console.log({
+      email,
+      username,
+      password,
+      location,
+    });
     models
       .getUserByEmail(email)
       .then((users) => {
         if (users.length === 0) {
           models
-            .insertNewUser({ email, username, password, location })
+            .insertNewUser({
+              email,
+              username,
+              password,
+              location,
+            })
             .then((result) => {
               console.log(result);
-              response.writeHead(200, { "content-type": "application/json" });
+              response.writeHead(200, { 'content-type': 'application/json' });
               response.end(
-                JSON.stringify({ msg: "User succesfully added", url: "/" })
+                JSON.stringify({ msg: 'User succesfully added', url: '/' })
               );
             })
             .catch((err) => {
               console.error(err);
-              response.writeHead(302, { location: "/serverError" });
+              response.writeHead(302, { location: '/serverError' });
               response.end();
             });
           return;
         }
-        response.writeHead(206, { "content-type": "application/json" });
+        response.writeHead(206, { 'content-type': 'application/json' });
         response.end(
-          JSON.stringify({ msg: "User exist, please use another email... " })
+          JSON.stringify({ msg: 'User exist, please use another email... ' })
         );
       })
       .catch((err) => {
         console.log(err);
-        response.writeHead(302, { location: "/serverError" });
+        response.writeHead(302, { location: '/serverError' });
         response.end();
       });
   });
-  request.on("error", (err) => {
+  request.on('error', (err) => {
     console.error(err);
-    response.writeHead(302, { location: "/serverError" });
+    response.writeHead(302, { location: '/serverError' });
     response.end();
   });
 }
